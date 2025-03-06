245deafe18cfd6d0e10144018e14f863c34827a5
Astha Thummar <asthathummar0501@gmail.com>
2025-03-06 17:14:00 +0530

    Add files via upload

---
+++ amds.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("enter a:");
+    scanf("%d",&a);
+    printf("enter b:");
+    scanf("%d",&b);
+
+    printf("add=%d\n",a+b);
+
+    printf("sub=%d\n",a-b);
+
+    printf("mul=%d\n",a*b);
+
+    printf("div=%d\n",a/b);
+}
---
+++ areatri.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    float h,b;
+    printf("entet h:");
+    scanf("%f",&h);
+    printf("entet b:");
+    scanf("%f",&b);
+    printf("area=%f",h*b);
+
+}
