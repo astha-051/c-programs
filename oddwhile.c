7345dd3acf4c27bcbcbeb48251385c4a0ac5f725
Astha Thummar <asthathummar0501@gmail.com>
2025-06-18 17:15:08 +0530

    Add files via upload

---
+++ oddwhile.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int i=1;
+    int n;
+    printf("enter n:");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        if(n%2!=0)
+        {
+            printf("%d",i);
+        }
+        else
+        {
+            printf("%d",i);
+        }
+        i++;
+    }
+
+}
---
+++ outputhelp.c
@@ -0,0 +1,15 @@
+ #include <stdio.h>
+ void main()
+ {
+    printf("includehelp.com\rOK\n");
+    printf("includehelp.com\b\b\bOk\n");
+ 
+ 
+    unsigned char c=290;
+    printf("%d",c);
+ 
+ 
+    int ok=-100;
+    -100;
+    printf("%d",ok);
+ }
