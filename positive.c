7e1e6d4f92b4269c4d13c5a2b63f17331b73519e
Astha Thummar <asthathummar0501@gmail.com>
2025-07-02 16:46:21 +0530

    Add files via upload

---
+++ positive.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter a:");
+    scanf("%d",&a);
+    if(a>0)
+    {
+        printf("%d is positive");
+    }
+    else
+    {
+        printf("%d is nagative");
+    }
+}
