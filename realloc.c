aea4d829e06e6e4abf327f6d833081b9d11e6938
Astha Thummar <asthathummar0501@gmail.com>
2025-07-12 14:38:39 +0530

    Add files via upload

---
+++ realloc.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    int *fp;
+    fp=(int *)malloc(sizeof(int));
+    *fp=25;
+    fp=(int *)realloc(fp,2*sizeof(int));
+    printf("%d",fp);
+    free(fp);
+}
